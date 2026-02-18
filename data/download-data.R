212c111f1dad3fcba8b0e50c69c84e2d47411fa4
WillemvdMerbel <W.n.vdmerbel@tilburguniversity.edu>
2026-02-18 12:08:50 +0100

    Adding # lines with nessescary installs for loading the data

--- data/download-data.R
+++ data/download-data.R
@@ -1,10 +1,11 @@
 # This script will be used to populate the \data directory
 # with all necessary raw data files.
 
+#Run the installs below if you don't have these R packages yet (by removing the # in front of the lines)
+#install.packages("googledrive")
+#install.packages(tidyverse)
 
 #Below is the code to download the relevant yelp dataset, note that you need to choose 1 or 2 in the console to login to google drive to get the authorization to download the data
-
-
 suppressWarnings(library(googledrive))
 file_id <- "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x" 
 drive_deauth()  # clear old token
