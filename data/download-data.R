0dc7598653ba1dedae37ebb85d2a88e406ece94c
WillemvdMerbel <W.n.vdmerbel@tilburguniversity.edu>
2026-03-03 15:38:35 +0100

    Adding the second dataset to download file

--- data/download-data.R
+++ data/download-data.R
@@ -5,7 +5,7 @@
 #install.packages("googledrive")
 #install.packages(tidyverse)
 
-#Below is the code to download the relevant yelp dataset, note that you need to choose 1 or 2 in the console to login to google drive to get the authorization to download the data
+#Below is the code to download the relevant yelp datasets, note that you need to choose 1 or 2 in the console to login to google drive to get the authorization to download the data
 suppressWarnings(library(googledrive))
 file_id <- "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x" 
 drive_deauth()  # clear old token
@@ -13,3 +13,10 @@ drive_auth(scopes = "drive.readonly")
 drive_download(as_id(file_id), path = "users.csv", overwrite = TRUE)
 library(tidyverse)
 Dataset_raw <- read_csv("users.csv")
+
+#run this as well to load the second data set
+file_id2 <- "1iOHDfm7y57dXkM1FoBZlZB3DbnUBXbYD" 
+drive_auth(scopes = "drive.readonly")
+drive_download(as_id(file_id2), path = "tips.csv", overwrite = TRUE)
+tips <- read_csv("tips.csv")
+
