a2b9d3056e649eb1db36e94b7f1a69b96bb50905
WillemvdMerbel <W.n.vdmerbel@tilburguniversity.edu>
2026-03-18 16:35:29 +0100

    updated download to automatically set working directory using here, to avoid errors

--- src/data-preparation/download-data.R
+++ src/data-preparation/download-data.R
@@ -7,17 +7,15 @@
 
 library(googledrive)
 library(tidyverse)
+library(here)
 
-# Deauthorize to avoid login prompts
 drive_deauth()
 
-# List of files to download: each element is a named list with id and target path
 files_to_download <- list(
-  list(id = "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x", path = "data/users.csv"),
-  list(id = "1iOHDfm7y57dXkM1FoBZlZB3DbnUBXbYD", path = "data/tips.csv")
+  list(id = "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x", path = here("data", "users.csv")),
+  list(id = "1iOHDfm7y57dXkM1FoBZlZB3DbnUBXbYD", path = here("data", "tips.csv"))
 )
 
-# Loop over each file and download
 for (file in files_to_download) {
   drive_download(
     as_id(file$id),
